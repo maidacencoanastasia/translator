62fc23032700c0be250f8b45fc71f5fe37c0b3c9
maidacencoanastasia <maidacenco100@gmail.com>
2021-12-13 12:37:27 +0200

    solve file.delete()

--- src/main/java/com/example/translator/repository/WordTranslatorRepository.java
+++ src/main/java/com/example/translator/repository/WordTranslatorRepository.java
@@ -4,6 +4,7 @@ import com.example.translator.model.Defenition;
 import com.example.translator.model.Word;
 import com.google.gson.Gson;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.Reader;
@@ -13,8 +14,9 @@ import java.nio.file.Paths;
 
 public class WordTranslatorRepository {
     private Gson gson = new Gson();
-    public String translateWord(String word, String language){
-        String fileName = "src/main/resources/translations/" +  language + "/"  + word + ".json";
+
+    public String translateWord(String word, String language) {
+        String fileName = "src/main/resources/translations/" + language + "/" + word + ".json";
         try {
             Reader reader = Files.newBufferedReader(Paths.get(fileName));
             Word wordModel = gson.fromJson(reader, Word.class);
@@ -25,58 +27,96 @@ public class WordTranslatorRepository {
         }
     }
 
-    public boolean addWord(Word word, String language){
-        String fileName = "src/main/resources/translations/" +  language + "/"  + word.word + ".json";
+    public boolean addWord(Word word, String language) {
+        String fileName = "src/main/resources/translations/" + language + "/" + word.word + ".json";
         try {
             Writer writer = new FileWriter(fileName);
             gson.toJson(word, writer);
             writer.close();
-        } catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
 
-    public boolean deleteWord(String word, String language){
-        String fileName = "src/main/resources/translations/" +  language + "/"  + word + ".json";
+    public boolean deleteWord(String word, String language) {
+        String fileName = "src/main/resources/translations/" + language + "/" + word + ".json";
+
         try {
-            File file = new File(fileName);
-
-            boolean flag = file.createNewFile();
-            if (flag) {
-                System.out.println("File has been created successfully at the specified location");
-            }
-            else {
-                System.out.println("File already present at the specified location");
-            }
-
-            file.delete();//  метод работает не корректно, но удаление выполняет
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public boolean addDefinitionForWord(String word, String language, Defenition definition){
-        String fileName = "src/main/resources/translations/" +  language + "/"  + word + ".json";
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(fileName));
-            Word wordModel = gson.fromJson(reader, Word.class);
-            reader.close();
-            wordModel.definitions.add(definition);//todo
-            try {
-                Writer writer = new FileWriter(fileName);
-                gson.toJson(wordModel, writer);
-                writer.close();
-            } catch (Exception e){
+            File f = new File(fileName); //file to be delete
+            if (f.delete()) //returns Boolean value
+            {
+                System.out.println("File " + f.getName() + " is deleted");
+                return true;
+                //getting and printing the file name
+            } else {
+                System.out.println("Delete operation failed");
                 return false;
             }
-            return true;
         } catch (Exception e) {
+            e.printStackTrace();
             return false;
         }
+    }
+//        try {
+//            //constructor of file class having file as argument
+//            File file = new File(fileName);
+//            if (!Desktop.isDesktopSupported())
+//            //check if Desktop is supported by Platform or not
+//            {
+//                System.out.println("not supported");
+//                //return true;
+//            }
+//            Desktop desktop = Desktop.getDesktop();
+//            if (file.exists()) //checks file exists or not
+//                file.delete(); //desktop.open(file); //opens the specified file
+//            return true;
+////            file.delete();
+//        } catch (Exception e) {
+//            System.out.println("not such file");
+//            e.printStackTrace();
+//        }
+//    return false;
+//    }
+        //////////////////
+//        try {
+//            File file = new File(fileName);
+//
+//            boolean flag = file.createNewFile();
+//            if (flag) {
+//                System.out.println("File has been created successfully at the specified location");
+//            }
+//            else {
+//                System.out.println("File already present at the specified location");
+//            }
+//
+//            file.delete();//  метод работает не корректно, но удаление выполняет
+//            return true;
+//        } catch (Exception e) {
+//            return false;
+//        }
+//    }
+
+        public boolean addDefinitionForWord (String word, String language, Defenition definition){
+            String fileName = "src/main/resources/translations/" + language + "/" + word + ".json";
+            try {
+                Reader reader = Files.newBufferedReader(Paths.get(fileName));
+                Word wordModel = gson.fromJson(reader, Word.class);
+                reader.close();
+                wordModel.definitions.add(definition);//todo
+                try {
+                    Writer writer = new FileWriter(fileName);
+                    gson.toJson(wordModel, writer);
+                    writer.close();
+                } catch (Exception e) {
+                    return false;
+                }
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+
+        }
+
 
     }
-
-
-}
